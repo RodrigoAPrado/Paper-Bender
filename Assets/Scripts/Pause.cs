f3797b865e39b4eddaa6fda43b54e83d60e51488
aCantarelli <andrecantarelli@hotmail.com>
2014-09-21 23:52:07 -0300

    debug

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour 
+{
+    public bool isPaused = false;
+    public string levelToBeLoaded;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update () 
+    {
+	    if(Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused;
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            Application.LoadLevel(levelToBeLoaded);
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 1;
+            Application.LoadLevel("Level_Select");
+        }
+
+        if (isPaused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+	}
+}
---
+++ Assets/Scripts/TheFuckingEnd.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheFuckingEnd : MonoBehaviour 
+{
+    public GameObject msgLevelClear;
+    public bool msgActivate = false;
+    public bool iniciarCountdown = false;
+    public float count = 3;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        if (iniciarCountdown)
+            EndGame();
+	}
+
+    void OnTriggerEnter2D()
+    {
+        iniciarCountdown = true;
+    }
+    
+    void EndGame()
+    {
+        if (!msgActivate)
+        {
+            msgActivate = !msgActivate;
+            msgLevelClear.SetActive(true);
+        }
+        count -= Time.deltaTime;
+        if (count < 0)
+        {
+            Application.LoadLevel("Level_Select");
+        }
+    }
+}
