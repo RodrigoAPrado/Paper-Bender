4eddd8dd7db9b96c3326877334f7f0b3d2dea031
aCantarelli <andrecantarelli@hotmail.com>
2014-09-21 22:17:03 -0300

    menu

---
+++ Assets/Menu/Scripts/ActivateOnClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateOnClick : MonoBehaviour 
+{
+    public GameObject target;
+
+	void Start () 
+    {
+	    
+	}
+	
+	void Update () 
+    {
+	    
+	}
+
+    void OnClick()
+    {
+        print("lalal");
+        target.SetActive(true);
+    }
+}
---
+++ Assets/Menu/Scripts/button_Quit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button_Quit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnClick()
+    {
+        Application.Quit();
+    }
+}
