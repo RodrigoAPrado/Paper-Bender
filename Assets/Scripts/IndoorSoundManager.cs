cbd38c90c345a1dc9617a19caf79cff16a787dee
SoulBuster <rodrigo.almprado@gmail.com>
2014-09-29 23:20:49 -0300

    fui cabaco e esqueci de dar add nuns negocio

---
+++ Assets/Scripts/IndoorSoundManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndoorSoundManager : MonoBehaviour {
+
+	public Transform limit;
+	public bool indoors;
+	public LayerMask player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		indoors = Physics2D.OverlapArea(transform.position, limit.position, player);
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	
+	public IndoorSoundManager[] iSM;
+	bool indoor;
+	public AudioSource indoorMusic;
+	public AudioSource outdoorMusic;
+	float volumeOutDoor;
+	[SerializeField] bool startsIndoor;
+	// Use this for initialization
+	void Start () {
+		if(startsIndoor)
+			volumeOutDoor = 0;
+		else
+			volumeOutDoor = 1;
+		indoorMusic.volume = volumeOutDoor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print("IndoorMusic: " + indoorMusic.time);
+		print("OutdoorMusic: " + outdoorMusic.time);
+		if(outdoorMusic.time > 20)
+		{
+			outdoorMusic.time = 0;
+		}
+		if(indoorMusic.time != outdoorMusic.time)
+		{
+			indoorMusic.time = outdoorMusic.time;
+		}
+		if(!indoor)
+		{
+			if(volumeOutDoor < 1)
+			{
+				volumeOutDoor += Time.deltaTime * 2;
+			}
+			else
+			{
+				volumeOutDoor = 1;
+			}
+		}
+		else
+		{
+			if(volumeOutDoor > 0)
+			{
+				volumeOutDoor -= Time.deltaTime * 2;
+			}
+			else
+			{
+				volumeOutDoor = 0;
+			}
+		}
+		indoorMusic.volume = volumeOutDoor;
+		for(int i = 0; i < iSM.Length; i++)
+		{
+			if(iSM[i].indoors)
+			{
+				indoor = true;
+				return;
+			}
+		}
+		indoor = false;
+	}
+}
