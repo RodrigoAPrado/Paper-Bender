187a12fb4f8b7702d820e0fc85caf9bd839d89a9
SoulBuster <rodrigo.almprado@gmail.com>
2014-10-20 02:15:05 -0200

    camera

--- Assets/Scripts/UltraRangeStatueController.cs
+++ Assets/Scripts/UltraRangeStatueController.cs
@@ -7,14 +7,62 @@ public class UltraRangeStatueController : MonoBehaviour {
 	[SerializeField] int cameraRestriction;
 	public CircleCollider2D range;
 	public BoxCollider2D ultraRange;
+	public Transform left;
+	public Transform right;
+	public LayerMask player;
+	bool working;
+	public SpriteRenderer light;
+	float lightTime;
+	bool increase;
 	// Use this for initialization
 	void Start () {
 		cM = Camera.main.GetComponent<CameraMovement>();
+		lightTime = 0;
+		Color c = new Color(1,1,1,lightTime);
+		light.color = c;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(cameraRestriction == cM.currentCameraPosition)
+		if(Physics2D.OverlapArea(left.position, right.position, player))
+   	    {
+			range.enabled = false;
+			ultraRange.enabled = true;
+			working = true;
+		}
+		else
+		{
+			ultraRange.enabled = false;
+			range.enabled = true;
+			working = false;
+		}
+		if(working)
+		{
+			if(increase)
+			{
+				if(lightTime < 1)
+					lightTime += Time.deltaTime;
+				else
+					increase = false;
+			}
+			else
+			{
+				if(lightTime > 0)
+					lightTime -= Time.deltaTime;
+				else
+					increase = true;
+			}
+		}
+		else
+		{
+			if(lightTime > 0)
+				lightTime -= Time.deltaTime * 0.5f;
+			else
+				lightTime = 0;
+		}
+		Color c = new Color(1,1,1,lightTime);
+		light.color = c;
+		/*if(cameraRestriction == cM.currentCameraPosition)
 		{
 			range.enabled = false;
 			ultraRange.enabled = true;
@@ -23,6 +71,6 @@ public class UltraRangeStatueController : MonoBehaviour {
 		{
 			ultraRange.enabled = false;
 			range.enabled = true;
-		}
+		}*/
 	}
 }
