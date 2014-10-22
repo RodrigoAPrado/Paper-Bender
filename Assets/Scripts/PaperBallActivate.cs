6afab020c7dd5f611abf70af4b7f4b9270b1e541
SoulBuster <rodrigo.almprado@gmail.com>
2014-10-22 15:22:08 -0200

    paperball

---
+++ Assets/Scripts/PaperBallActivate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaperBallActivate : MonoBehaviour {
+
+	public GameObject paper;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(paper.activeSelf)
+		{
+			GetComponent<Rigidbody2D>().isKinematic = false;
+		}
+	}
+}
