9edad941ef96fa6281f56c32c32a3cdcf4c69cf3
aCantarelli <andrecantarelli@hotmail.com>
2014-10-22 17:46:28 -0200

    nao da conflit na cena pls

---
+++ Assets/Scripts/ClearBandit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearBandit : MonoBehaviour 
+{
+    public GameObject bandidao;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        if (bandidao == null)
+        { 
+            print("ACABOOOOU");
+            Application.LoadLevel("Level_Select");
+        }
+	}
+}
