251b7a090325b8fce745b87a89bf4316524c1784
SoulBuster <rodrigo.almprado@gmail.com>
2014-11-09 00:19:57 -0200

    dialogo

--- Assets/Scripts/StartTalk.cs
+++ Assets/Scripts/StartTalk.cs
@@ -6,14 +6,28 @@ public class StartTalk : MonoBehaviour {
 	NPCDialogController nPC;
 	public string[] thisText;
 	public NPCDialogController.allAvatar thisAvatar;
+	SpriteRenderer sRenderer;
+	BoxCollider2D bCollider;
 	// Use this for initialization
 	void Start () {
 		nPC = GameObject.FindGameObjectWithTag("ChatBox").GetComponent<NPCDialogController>();
+		bCollider = GetComponent<BoxCollider2D>();
+		sRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if(nPC.onChat)
+		{
+			bCollider.enabled = false;
+			sRenderer.enabled = false;
+		}
+		else
+		{
+			bCollider.enabled = true;
+			sRenderer.enabled = true;
+		}
 	}
 	public void SetTalkOn()
 	{
