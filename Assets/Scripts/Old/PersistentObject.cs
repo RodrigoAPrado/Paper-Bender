98b48e1c9df4a6e7bc600b7f7c7ce4c079490fd3
PhyroPhyro <jpaulo.gxavier@gmail.com>
2014-11-22 02:13:34 -0200

    Menu novo, musica menu, mais chao

---
+++ Assets/Scripts/Old/PersistentObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistentObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	void OnLevelWasLoaded(int scene)
+	{
+		if(Application.loadedLevelName != "Menu" && Application.loadedLevelName != "Level_Select" && Application.loadedLevelName != "Save_Select")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
