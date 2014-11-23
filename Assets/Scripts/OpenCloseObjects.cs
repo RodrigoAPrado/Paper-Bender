4913315b93e74274a0e4ef82f42f6a7896a5ac60
PhyroPhyro <jpaulo.gxavier@gmail.com>
2014-11-23 03:32:19 -0200

    Options (layout)

---
+++ Assets/Scripts/OpenCloseObjects.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenCloseObjects : MonoBehaviour {
+
+	public GameObject ObjectClose;
+
+	public enum tabOpenCloseObj
+	{
+		Open,
+		Close
+	}
+
+	public tabOpenCloseObj openOrClose;
+
+	void OnClick()
+	{
+		if(openOrClose == tabOpenCloseObj.Close)
+			ObjectClose.SetActive(false);
+		else
+			ObjectClose.SetActive(true);
+	}
+}
