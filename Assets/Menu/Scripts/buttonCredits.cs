3e2f1d34cac2b28331094793fae50d1b83d47271
aCantarelli <andrecantarelli@hotmail.com>
2014-11-23 02:16:15 -0200

    fodeu

---
+++ Assets/Menu/Scripts/buttonCredits.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonCredits : MonoBehaviour 
+{
+    public Transform buttons;
+    public Transform title;
+    public Transform credits;
+    public Transform creditsBack;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void OnClick()
+    {
+        title.gameObject.SetActive(false);
+        credits.gameObject.SetActive(true);
+        creditsBack.gameObject.SetActive(true); 
+        buttons.gameObject.SetActive(false);
+    }
+
+}
